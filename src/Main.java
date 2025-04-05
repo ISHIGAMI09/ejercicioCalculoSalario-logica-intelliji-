5dca5a5c6a8bcf9207b55bdec845e4d9192341d5
ishigami09 <paulinalondonodiaz01@gmail.com>
2025-04-05 14:25:19 -0500

    ejerccio para calcular salaro de un trabajador

---
+++ src/Main.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+
+public class Main {
+ public static void main(String[] args) {
+     Scanner sc = new Scanner(System.in);
+     String nombreEmpleado = "";
+     String documentoEmpleado = "";
+     double salarioBruto = 0;
+     double salarioNeto = 0;
+     double valorHoras = 0;
+     double catidadHoras = 0;
+     double deduccionSalud = 0;
+     double deduccionPenssion = 0;
+     double horasExtras = 0;
+     double valorHorasExtras = 0;
+     double bonificacion = 0;
+     double fondoSolidaridadPensional = 0;
+     double valorReteFuente = 0;
+     double auxilioDeTransporte = 0;
+     double salarioMinomo = 0;
+
+     System.out.println("Ingresa el Nombre del Empleado");
+     nombreEmpleado = sc.nextLine();
+     System.out.println("Ingrese el documento del empleado");
+     documentoEmpleado = sc.nextLine();
+     System.out.println("Ingrese el valor de la hora");
+     valorHoras = sc.nextDouble();
+     System.out.println("Ingrese la cantidad de las horas");
+     catidadHoras = sc.nextDouble();
+     System.out.println("Ingrese el valor del salario minimo actual: ");
+     salarioMinomo = sc.nextDouble();
+
+     if(catidadHoras > 24){
+         System.out.println("Se le puede pagar al empleado");
+         salarioBruto = valorHoras * catidadHoras ;
+         System.out.println("el salario bruto del empleado: "+nombreEmpleado + " es  $:"+ salarioBruto);
+         deduccionPenssion = salarioBruto * 0.04 ;
+         deduccionSalud = salarioBruto * 0.04 ;
+         System.out.println("La decuccion por salud es:" + deduccionSalud);
+         System.out.println("La decuccion por pension es:" + deduccionPenssion);
+         if (salarioBruto <= (salarioMinomo*2)) {
+             auxilioDeTransporte = 2000000;
+             bonificacion = salarioBruto * 0.1;
+         } else {
+             auxilioDeTransporte = 0;
+             bonificacion = 0 ;
+         }
+         if (catidadHoras > 96 ){
+         horasExtras = catidadHoras * 96;
+         valorHorasExtras = (valorHoras * 0.25) * horasExtras;
+         }
+         System.out.println("Detalle de pago" + nombreEmpleado);
+         System.out.println("El salario bruto del empleado" + nombreEmpleado + " es $");
+         System.out.println("la deduccion por salud es " + deduccionSalud);
+         System.out.println("la deduccion por pension es" + deduccionPenssion);
+         System.out.println(" el auxilio de transporte es " + auxilioDeTransporte);
+         System.out.println(" la bonificacion es " + bonificacion);
+         System.out.println("el pago de horas extras es " + horasExtras);
+         System.out.println(" el salario neto es" + (salarioBruto-deduccionSalud-deduccionPenssion+auxilioDeTransporte+bonificacion+horasExtras));
+     } else {
+         System.out.println("le faltan" + (24- catidadHoras) + "horas de trabajo");
+
+
+
+      }
+    }
+}
